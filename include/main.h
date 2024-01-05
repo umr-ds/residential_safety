8e6666895b5dc51c5b53bdbab433d9d1e8beceab
Kevin Lüttge <69243143+wasanderes94@users.noreply.github.com>
2024-01-05 19:46:16 +0100

    feat: add main.h

--- include/main.h
+++ include/main.h
@@ -1,4 +1,20 @@
 #ifndef MASTERTHESIS_MAIN_H
 #define MASTERTHESIS_MAIN_H
 
+#include "sensor_reader.h"
+#include "communication.h"
+
+#define NUM_CALIBRATION_VALUES 300
+
+uint8_t node_id = 0;
+
+float mean_odor = 0;
+float stdDev_odor = 0;
+float mean_CO = 0;
+float stdDev_CO = 0;
+float mean_temperature = 0;
+float stdDev_temperature = 0;
+Acceleration mean_accel;
+Acceleration stdDev_accel;
+
 #endif //MASTERTHESIS_MAIN_H
