7dc3e575e1efbf87f6802188165dc7baa7a5b0de
Kevin Lüttge <69243143+wasanderes94@users.noreply.github.com>
2024-01-17 14:27:35 +0100

    feat: add ulp assembly files for deepsleep wakeup

--- include/main.h
+++ include/main.h
@@ -4,18 +4,21 @@
 #include "sensor_reader.h"
 
 #define NUM_CALIBRATION_VALUES 300
+#define NUM_SENSORS 2
+
 #define LEAKAGE_FLAG 1
 #define PIR_FLAG 2
 #define HALL_FLAG 3
 
-uint8_t node_id = 0;
-float mean_odor = 0;
-float stdDev_odor = 0;
-float mean_CO = 0;
-float stdDev_CO = 0;
-float mean_temperature = 0;
-float stdDev_temperature = 0;
-Acceleration mean_accel;
-Acceleration stdDev_accel;
+
+RTC_DATA_ATTR uint8_t node_id = 0;
+RTC_DATA_ATTR float mean_odor = 0;
+RTC_DATA_ATTR float stdDev_odor = 0;
+RTC_DATA_ATTR float mean_CO = 0;
+RTC_DATA_ATTR float stdDev_CO = 0;
+RTC_DATA_ATTR float mean_temperature = 0;
+RTC_DATA_ATTR float stdDev_temperature = 0;
+RTC_DATA_ATTR Acceleration mean_accel;
+RTC_DATA_ATTR Acceleration stdDev_accel;
 
 #endif //MASTERTHESIS_MAIN_H
---
+++ ulp/ulp_config.h
@@ -0,0 +1,11 @@
+#ifndef MASTERTHESIS_ULP_CONFIG_H
+#define MASTERTHESIS_ULP_CONFIG_H
+
+
+#pragma once
+
+/* Set low and high thresholds, approx. 1.35V - 1.75V*/
+#define CO_ADC_CHANNEL 6
+#define ODOR_ADC_CHANNEL 3
+
+#endif //MASTERTHESIS_ULP_CONFIG_H
